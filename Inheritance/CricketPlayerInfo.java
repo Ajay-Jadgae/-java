ec95500a68b77ca06d90f9f2aef36491f99fa8d2
jadgae0848 <ajayjadagae.xworkz@gmail.com>
2025-09-23 15:30:34 +0530

    inheritance

---
+++ Inheritance/CricketPlayerInfo.java
@@ -0,0 +1,34 @@
+class CricketPlayerInfo {
+    public static void name() {
+        System.out.println("Virat Kohli");
+    }
+    public static void team() {
+        System.out.println("India");
+    }
+    public static void role() {
+        System.out.println("Batsman");
+    }
+    public static void battingStyle() {
+        System.out.println("Right-hand bat");
+    }
+    public static void bowlingStyle() {
+        System.out.println("Right-arm medium");
+    }
+    public static void matches() {
+        System.out.println("500+");
+    }
+    public static void runs() {
+        System.out.println("26000+");
+    }
+    public static void centuries() {
+        System.out.println("80+");
+    }
+    public static void awards() {
+        System.out.println("ICC Player of the Decade");
+    }
+    public static void jerseyNumber() {
+        System.out.println("18");
+    }
+}
+
+
---
+++ Inheritance/CricketPlayerInfoRunner.java
@@ -0,0 +1,14 @@
+class CricketPlayerInfoRunner {
+    public static void main(String[] args) {
+        CricketPlayerInfo.name();
+        CricketPlayerInfo.team();
+        CricketPlayerInfo.role();
+        CricketPlayerInfo.battingStyle();
+        CricketPlayerInfo.bowlingStyle();
+        CricketPlayerInfo.matches();
+        CricketPlayerInfo.runs();
+        CricketPlayerInfo.centuries();
+        CricketPlayerInfo.awards();
+        CricketPlayerInfo.jerseyNumber();
+    }
+}
