1755e9fc63044e5f33e0843d3e92f88cc34b4941
jadgae0848 <ajayjadagae.xworkz@gmail.com>
2025-10-01 22:11:51 +0530

    java practice

---
+++ New folder/Collage.java
@@ -0,0 +1,8 @@
+class Collage {
+    public String collageName = "Sunrise Collage";
+
+    public void showDetails(Student student) {
+        student.printName();  
+        System.out.println("Collage Name = " + collageName);
+    }
+}
---
+++ New folder/CollageRunner.java
@@ -0,0 +1,8 @@
+class CollageRunner {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Collage collage = new Collage();
+
+        collage.showDetails(student);
+    }
+}
