2b0fdbf929b8f49a75cd9c2690bb362ad8f578d5
jadgae0848 <ajayjadagae.xworkz@gmail.com>
2025-10-04 23:14:19 +0530

    method chaining

---
+++ Methods/Wire.java
@@ -0,0 +1,14 @@
+class Wire{
+public static void price(){
+System.out.println("5000");
+}
+public static void capacity(){
+price();
+System.out.println("120wt");
+}
+public static void factory(){
+capacity();
+System.out.println("havai");
+}
+
+}
---
+++ Methods/WireRunner.java
@@ -0,0 +1,7 @@
+class WireRunner{
+public static void main(String [] Args){
+Wire.price();
+Wire.capacity();
+Wire.factory();
+}
+}
